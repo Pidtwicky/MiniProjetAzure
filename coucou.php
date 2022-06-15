e1461297a66c3b20118c6b4105bde2c91b96ffac
Pidtwicky <clementramel@gmail.com>
2022-06-15 14:49:42 +0200

    test

---
+++ coucou.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exemple</title>
+    </head>
+    <body>
+
+        <?php
+            echo "Coucou moi !";
+        ?>
+
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
